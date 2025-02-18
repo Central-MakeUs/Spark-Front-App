87165905f16b6c28b2ffa3173ae81066f583bb69
Paul Yang <sea5122916@gmail.com>
2025-02-18 17:13:14 +0900

    feat: 웹뷰 연결

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,93 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { WebView } from "react-native-webview";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
+import { SplashScreen } from "expo-router";
+import Splash from "@/components/Splash";
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+  Platform,
+  BackHandler,
+} from "react-native";
+import { useEffect, useRef, useState } from "react";
+import "react-native-reanimated";
+import Constants from "expo-constants";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  SplashScreen.hideAsync();
+  const [loading, setLoading] = useState(true);
+  const webViewRef = useRef<WebView>(null);
+
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (Platform.OS === "android") {
+      BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener(
+          "hardwareBackPress",
+          onAndroidBackPress
+        );
+      };
     }
-  }, [loaded]);
+  }, []);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+  return loading ? (
+    <>
+      <Splash setFinisehd={() => setLoading(false)} />
+    </>
+  ) : (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <View style={styles.container}>
+        <StatusBar barStyle={"default"} animated={true} />
+        <WebView
+          ref={webViewRef}
+          source={{
+            uri: "https://www.app-spark.shop/result",
+          }}
+          javaScriptEnabled
+          bounces
+          scrollEnabled
+          mixedContentMode={"always"}
+          allowsBackForwardNavigationGestures
+          cacheEnabled={false}
+          onContentProcessDidTerminate={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.warn("Content process terminated, reloading", nativeEvent);
+            webViewRef.current?.reload();
+          }}
+        />
+      </View>
+    </SafeAreaProvider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  input: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "red",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+  },
+});
